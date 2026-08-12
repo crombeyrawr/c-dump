4bbdbe160bdedf0bd4e7729c4979d6835079e5af
crombey <croombeyzinhowo@gmail.com>
2026-08-12 02:51:43 -0300

    try harder

--- memory_page_size.c
+++ memory_page_size.c
@@ -2,9 +2,9 @@
 #include <windows.h>
 int main(){
 
-    SYSTEM_INFO SysInfo = { 0 }; // Passo crucial antes de usar a função GetSystemInfo()
-    GetSystemInfo(&SysInfo); // Obtem informaçoes sobre o Hardware da maquina.
-    printf("Memory Page Size of Hardware: %lu bytes\n", SysInfo.dwPageSize); // Retorna o tamanho da Memory Page das informaçoes do Hardware da maquina. (4 KB) / 4096 bytes
+    SYSTEM_INFO SysInfo = { 0 }; // Declaração da Structure que armazena informações sobre o Sistema Operacional da maquina
+    GetSystemInfo(&SysInfo); // Obtem informaçoes sobre o Sistema Operacional da maquina, e referencia aonde o endereço de memoria de SysInfo esta armazenado, pelo operador "&"
+    printf("Windows Memory Page Size: %lu bytes\n", SysInfo.dwPageSize); // Retorna o tamanho da Memory Page default do Windows (4 KB) / 4096 bytes
     return 0;
 
 }
