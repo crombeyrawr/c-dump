5c580583ba8090cd2333484e8fbd18cea64e35e4
crombey <croombeyzinhowo@gmail.com>
2026-08-12 03:11:12 -0300

    clarity moment ig

--- memory_page_size.c
+++ memory_page_size.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <windows.h>
 int main(){
-
-    SYSTEM_INFO SysInfo = { 0 }; // Declaração da Structure que armazena informações sobre o Sistema Operacional da maquina
-    GetSystemInfo(&SysInfo); // Obtem informaçoes sobre o Sistema Operacional da maquina, e referencia aonde o endereço de memoria de SysInfo esta armazenado, pelo operador "&"
+    SYSTEM_INFO SysInfo = { 0 }; // Declaração da Structure que armazena informações sobre o Sistema Operacional
+    GetSystemInfo(&SysInfo); // Obtem informaçoes sobre o Sistema Operacional, e referencia aonde o endereço de memoria de SysInfo esta armazenado, pelo operador "&"
     printf("Windows Memory Page Size: %lu bytes\n", SysInfo.dwPageSize); // Retorna o tamanho da Memory Page default do Windows (4 KB) / 4096 bytes
     return 0;
-
 }
